32efb3e91f10b3fc2c9869711c045c100679be3d
htmldev30 <htmldev30@gmail.com>
2023-02-15 23:14:18 -0700

    Backend JWT Authentication and Frontend Auth Setup

--- frontend/src/modules/AuthForms/ParentAuth1Form.js
+++ frontend/src/modules/AuthForms/ParentAuth1Form.js
@@ -2,14 +2,37 @@ import React, { useState } from 'react'
 import { VStack, Icon } from 'native-base'
 import { Feather } from '@expo/vector-icons'
 // Custom Imports
+import { axiosClient } from '../../../apiClient'
 import { CustomInput } from '../../components/customInput'
 import { CustomButton } from '../../components/customButton'
+import { storeUserInfo, storeUserJWT } from '../../shared/asyncStorage'
+import axios from 'axios'
 export const ParentAuth1Form = () => {
+    const [accountType, setAccountType] = useState('parent')
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    // TODO: Resolve error where Axios cannot connect to backend.
+    const handleSubmit = async () => {
+        console.log(firstName, lastName, email, password)
+        console.log('Clicked')
+        await axios({
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            method: 'get',
+            url: 'http://127.0.0.1:3000/',
+        })
+            .then((res) => {
+                console.log(res)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
     return (
         <VStack space={2.5} w="100%" pt="4" px="4">
             <CustomInput
@@ -71,7 +94,7 @@ export const ParentAuth1Form = () => {
                     />
                 }
             />
-            <CustomButton label="Sign Up" />
+            <CustomButton label="Sign Up" onPress={handleSubmit} />
         </VStack>
     )
 }
