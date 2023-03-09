212a4c38124cde37ee9537260fd9c63827f798ca
htmldev30 <htmldev30@gmail.com>
2023-03-08 17:12:07 -0700

    backend folder deleted

--- backend/routes/userAuthRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const userAuthRouter = express.Router()
-const userAuthController = require('../controllers/userAuth/userAuthController')
-
-userAuthRouter.post('/register', userAuthController.userRegistration)
-userAuthRouter.post('/login', userAuthController.userLogin)
-userAuthRouter.get('/logout', userAuthController.userLogout)
-module.exports = userAuthRouter
